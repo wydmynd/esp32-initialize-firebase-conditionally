03722509114c3b6f7a06cb65d79b93d6e14d0c1e
wydmynd <tomsofer@gmail.com>
2024-05-08 10:22:11 +0300

    objects initialized conditionally

--- src/main.cpp
+++ src/main.cpp
@@ -20,11 +20,11 @@
 #define TRIGGER_PIN 13
 
 // Define Firebase Data object
-FirebaseData stream;
-FirebaseData fbdo;
+FirebaseData *stream=NULL;
+FirebaseData *fbdo=NULL;
 
-FirebaseAuth auth;
-FirebaseConfig config;
+FirebaseAuth *auth=NULL;
+FirebaseConfig *config=NULL;
 
 unsigned long sendDataPrevMillis = 0;
 
@@ -36,11 +36,9 @@ bool config_mode = false;
 
 void setup()
 {
-    pinMode(TRIGGER_PIN, INPUT_PULLUP);
+  pinMode(TRIGGER_PIN, INPUT_PULLUP);
 
   Serial.begin(115200);
-
-
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
@@ -56,29 +54,37 @@ void setup()
 
   Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
 
-  /* Assign the api key (required) */
-  config.api_key = API_KEY;
-
-  /* Assign the user sign in credentials */
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
-
-  /* Assign the RTDB URL (required) */
-  config.database_url = DATABASE_URL;
-
   // To connect without auth in Test Mode, see Authentications/TestMode/TestMode.ino
-      if ( digitalRead(TRIGGER_PIN) == LOW) {
-        Serial.println("Config mode");
-        config_mode = true;
-    }
-    else {
-        Serial.println("Normal mode");
-        Firebase.begin(&config, &auth);
-        Firebase.reconnectWiFi(true);
-    }
+  if (digitalRead(TRIGGER_PIN) == LOW)
+  {
+    Serial.println("Config mode");
+    config_mode = true;
+  }
+  else
+  {
+    Serial.println("Normal mode");
 
+    /* Assign the api key (required) */
+    config = new FirebaseConfig();
+    config->api_key = API_KEY;
 
+    /* Assign the user sign in credentials */
+    auth = new FirebaseAuth();
+    auth->user.email = USER_EMAIL;
+    auth->user.password = USER_PASSWORD;
+
+    /* Assign the RTDB URL (required) */
+    config->database_url = DATABASE_URL;
+
+    /* Assign the Firebase objects */
+    stream = new FirebaseData();
+    fbdo = new FirebaseData();
+
+    Firebase.begin(config, auth);
+    Firebase.reconnectWiFi(true);
+  }
 }
+
 
 void loop()
 {
@@ -96,7 +102,7 @@ void loop()
       FirebaseJson json;
       json.add("data", "hello");
       json.add("num", count);
-      Serial.printf("Set json... %s\n\n", Firebase.RTDB.setJSON(&fbdo, "/counter/data/json", &json) ? "ok" : fbdo.errorReason().c_str());
+      Serial.printf("Set json... %s\n\n", Firebase.RTDB.setJSON(fbdo, "/counter/data/json", &json) ? "ok" : fbdo->errorReason().c_str());
     }
   } 
   else 
